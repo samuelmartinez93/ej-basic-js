6b9e4ee57f96b7990b5ebc7b71df75269dcc6210
Samuel MPD <samuelhoward1993@gmail.com>
2021-02-21 14:54:13 +0100

    primer ejercicio, primer intento

---
+++ ejerciciorgb.js
@@ -0,0 +1,14 @@
+let red = 100
+let green = 200
+let blue = 150
+
+coloresRgb(red, green, blue);
+
+
+function coloresRgb() {
+
+    console.log(`El color es R:${red}, G:${green} y B:${blue}`)
+
+};
+
+
