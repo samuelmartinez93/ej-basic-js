1dd12361f180b1d1aa8957e62a084d5ba7ba8b93
Samuel MPD <samuelhoward1993@gmail.com>
2021-02-21 16:40:25 +0100

    arreglado con llamada de funcion

--- ejerciciorgb.js
+++ ejerciciorgb.js
@@ -2,13 +2,10 @@ let red = 100
 let green = 200
 let blue = 150
 
-coloresRgb(red, green, blue);
-
-
-function coloresRgb() {
+function coloresRgb(red, green, blue) {
 
     console.log(`El color es R:${red}, G:${green} y B:${blue}`)
 
 };
 
-
+coloresRgb(red, green, blue);
